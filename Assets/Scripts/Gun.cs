eeba1779c4abf2559d5f27d9b1383f7e88bcc220
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-10-09 19:14:53 -0400

    redid pistol, shotgun, and assaultrifle scripts and added them to there guns

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -23,4 +23,4 @@ public class Gun : MonoBehaviour
 
     }
 }
-}
+
---
+++ Assets/Scripts/Shotgun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Gun
+{
+    override protected void Update()
+    {
+        base.Update();
+        // Shotgun & Pistol have semi-auto fire rate     
+        if (Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time; Fire();
+        }
+    }
+}
