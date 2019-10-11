037f8a1381adf622a8df2ae90bce32b5728b89b9
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-10-11 16:52:00 -0400

    Spawned in the robots and made a game script

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;// array of teleporters that spawn robots each wave
+    public int enemiesLeft;// tracks how many robots are still alive
+
+    // 1
+    void Start()
+    {
+        singleton = this;// inializes singleton
+        SpawnRobots();// spawns the robots
+    }
+    // 2
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();     enemiesLeft++;
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
