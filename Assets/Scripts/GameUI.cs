9c330747f5ce03fa3bb5882dc8b22583cbd03ea7
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-10-17 07:41:32 -0400

    added references to all the text commands in the gameUI script then added each text to there respectful places in the gameUI object in unity

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -5,10 +5,34 @@ using UnityEngine.UI;// allows us to access the base unity UI classes
 
 public class GameUI : MonoBehaviour
 {
-    [SerializeField] Sprite redReticle;// Allows you to convey information from C# runtime and particular classes 
-    [SerializeField] Sprite yellowReticle;// Sprite is an imported texture that is meant to be used in 2D and UI games
-    [SerializeField] Sprite blueReticle;
-    [SerializeField] Image reticle;// image is displaying the sprites to the screen
+    [SerializeField]
+    private Text ammoText;
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private Text armorText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text pickupText;
+    [SerializeField]
+    private Text waveText;
+    [SerializeField]
+    private Text enemyText;
+    [SerializeField]
+    private Text waveClearText;
+    [SerializeField]
+    private Text newWaveText;
+    [SerializeField]
+    Player player;
+    [SerializeField]
+    Sprite redReticle;// Allows you to convey information from C# runtime and particular classes 
+    [SerializeField]
+    Sprite yellowReticle;// Sprite is an imported texture that is meant to be used in 2D and UI games
+    [SerializeField]
+    Sprite blueReticle;
+    [SerializeField]
+    Image reticle;// image is displaying the sprites to the screen
 
     public void UpdateReticle()
     {
