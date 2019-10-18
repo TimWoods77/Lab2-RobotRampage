640fea146e61568fcdf91085095d2e6b8444ef1a
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-10-18 11:38:09 -0400

    Added a menu script and coded it to allow us to press the begin and exit buttons

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // 1 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");// loads battle scene
+    }
+    
+    // 2 
+    public void Quit()
+    {
+        Application.Quit();// exits the app unless your using it on unity
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
